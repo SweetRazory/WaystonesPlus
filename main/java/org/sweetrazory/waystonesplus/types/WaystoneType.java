806a8c409629da7bed549f573fb5af68e1d3921b
Milan <sacika200@gmail.com>
2023-06-26 03:21:33 +0200

    Bugfix 20230626

--- main/java/org/sweetrazory/waystonesplus/types/WaystoneType.java
+++ main/java/org/sweetrazory/waystonesplus/types/WaystoneType.java
@@ -1,5 +1,6 @@
 package org.sweetrazory.waystonesplus.types;
 
+import org.bukkit.Bukkit;
 import org.bukkit.inventory.ShapedRecipe;
 
 import java.util.List;
@@ -15,6 +16,8 @@ public class WaystoneType {
         this.blocks = blocks;
         BlockDisplays = blockDisplays;
         this.recipe = recipe;
+
+        Bukkit.getServer().addRecipe(recipe);
     }
 
     public String getTypeName() {
