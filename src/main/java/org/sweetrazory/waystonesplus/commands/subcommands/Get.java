84f7a6cb1affa1b872e78dffc0c9ab8e0ba7caa3
Milan <sacika200@gmail.com>
2023-06-30 19:14:14 +0200

    1.5.1

--- src/main/java/org/sweetrazory/waystonesplus/commands/subcommands/Get.java
+++ src/main/java/org/sweetrazory/waystonesplus/commands/subcommands/Get.java
@@ -3,8 +3,6 @@ package org.sweetrazory.waystonesplus.commands.subcommands;
 import org.bukkit.Color;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.sweetrazory.waystonesplus.enums.Visibility;
 import org.sweetrazory.waystonesplus.items.WaystoneSummonItem;
 import org.sweetrazory.waystonesplus.memoryhandlers.WaystoneMemory;
@@ -27,8 +25,7 @@ public class Get implements SubCommand {
                     // TODO: length handling
                     ItemStack skullItem = new WaystoneSummonItem().getLodestoneHead(name, args[1].toLowerCase(), null, null, Visibility.GLOBAL);
                     player.getInventory().addItem(skullItem);
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 10 * 20, 10));
-
+//                    player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 10 * 20, 10));
 //                    Bukkit.getScheduler().runTaskLater(WaystonesPlus.getInstance(), () -> player.removePotionEffect(PotionEffectType.BLINDNESS), 10 * 20); TODO: Invent animation
                 } else {
                     player.sendMessage(Color.ORANGE + "A waystone variation with that name doesn't exist!");
--- src/main/java/org/sweetrazory/waystonesplus/gui/inventory/screens/WaystoneSelector.java
+++ src/main/java/org/sweetrazory/waystonesplus/gui/inventory/screens/WaystoneSelector.java
@@ -3,7 +3,6 @@ package org.sweetrazory.waystonesplus.gui.inventory.screens;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
@@ -12,7 +11,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
-import org.sweetrazory.waystonesplus.WaystonesPlus;
 import org.sweetrazory.waystonesplus.enums.Visibility;
 import org.sweetrazory.waystonesplus.gui.inventory.MenuInventory;
 import org.sweetrazory.waystonesplus.utils.Console;
@@ -35,30 +33,30 @@ public class WaystoneSelector {
             Inventory clickedInventory = event.getClickedInventory();
             InventoryType.SlotType slotType = event.getSlotType();
 
-            if (clickedInventory != null && slotType != InventoryType.SlotType.OUTSIDE) {
+            ItemStack clickedItem = event.getCurrentItem();
+            if (clickedItem != null && clickedInventory != null && slotType != InventoryType.SlotType.OUTSIDE) {
                 try {
-                    ItemStack clickedItem = event.getCurrentItem();
+                    if (clickedItem.getItemMeta() == null) {
+                        return;
+                    }
                     if (clickedItem.getItemMeta().getDisplayName().equalsIgnoreCase("Previous") && clickedItem.getType().equals(Material.SNOWBALL)) {
                         menu.prevPage();
                     } else if (clickedItem.getItemMeta().getDisplayName().equalsIgnoreCase("Next") && clickedItem.getType().equals(Material.SNOWBALL)) {
                         menu.nextPage();
                     } else if (clickedItem.getType().equals(Material.BARRIER)) {
                         menu.close();
-                    } else {
-                        if (clickedItem.getItemMeta() == null) {
-                            return;
-                        }
+                    } else if (!clickedItem.getType().equals(Material.GRAY_STAINED_GLASS_PANE)) {
                         ItemMeta itemMeta = clickedItem.getItemMeta();
                         PersistentDataContainer dataContainer = itemMeta.getPersistentDataContainer();
 
-                        String visibility = dataContainer.get(Keys.waystoneVisibility, PersistentDataType.STRING); // TODO: Implement this
+                        String visibility = dataContainer.get(Keys.waystoneVisibility, PersistentDataType.STRING);
                         String world = dataContainer.get(Keys.waystoneWorld, PersistentDataType.STRING);
                         String owner = dataContainer.get(Keys.waystoneOwner, PersistentDataType.STRING);
                         Integer x = dataContainer.get(Keys.waystoneX, PersistentDataType.INTEGER);
                         Integer y = dataContainer.get(Keys.waystoneY, PersistentDataType.INTEGER);
                         Integer z = dataContainer.get(Keys.waystoneZ, PersistentDataType.INTEGER);
 
-                        if (visibility.equals(Visibility.PRIVATE.name()) && !player.hasPermission("waystonesplus.teleport.private") && !player.isOp() && !owner.equals(player.getUniqueId().toString())) {
+                        if (visibility != null && visibility.equals(Visibility.PRIVATE.name()) && !player.hasPermission("waystonesplus.teleport.private") && !player.isOp() && owner != null && !owner.equals(player.getUniqueId().toString())) {
                             return;
                         }
 
